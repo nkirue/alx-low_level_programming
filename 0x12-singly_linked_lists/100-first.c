a8f9d6bccb61f4e02c669956e1ecf60a8facf2d1
Nkiru Ezefosie <nkiru.happiness@gmail.com>
2023-07-28 20:54:30 -0700

    C - Singly linked lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,7 +3,7 @@
 void first_print(void) __attribute__ ((constructor));
 
 /**
- * first - This func prints a sentence before the main
+ * first_print - This func prints a sentence before the main
  * Function is executed
  */
 void first_print(void)
