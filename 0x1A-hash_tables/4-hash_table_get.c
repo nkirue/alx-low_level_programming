8a9e29f46edcd012556e75c200eb5fc0b293a3b3
Nkiru Ezefosie <nkiru.happiness@gmail.com>
2023-10-06 12:51:57 -0700

    C - Hash tables

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - To retrieve value associeted with key
+ * @ht: Pointer to hash table
+ * @key: Key to retrive value
+ *
+ * Return: Value or NULL if key does not exit.
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = 0;
+	hash_node_t  *bucket;
+
+	if (!ht || !key || !*key)
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	bucket = ht->array[index];
+
+	while (bucket)
+	{
+		if (!strcmp(key, bucket->key))
+			return (bucket->value);
+		bucket = bucket->next;
+	}
+	return (NULL);
+}
